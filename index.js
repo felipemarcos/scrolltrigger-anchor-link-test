8e62835785f595fad24284606f2215c9f1b0ebb5
Felipe Marcos <felipemarcosdos@gmail.com>
2023-09-15 13:22:40 -0300

    simplify

--- index.js
+++ index.js
@@ -7,14 +7,5 @@ addEventListener("DOMContentLoaded", () => {
     endTrigger: ".container",
     toggleClass: "is-sticky",
     pinSpacing: false,
-    onUpdate: (self) => {
-      if (self.getVelocity() === 0) return;
-      this.element.classList.add(this.animatingClass);
-
-      if (delayedCall) delayedCall.kill();
-      delayedCall = gsap.delayedCall(0.3, () => {
-        this.element.classList.remove(this.animatingClass);
-      });
-    },
   });
 });
